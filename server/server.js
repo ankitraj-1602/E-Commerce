e9ef6d06ba9469e1f49774b8af7520674bf13449
ankitraj-1602 <nycankit@gmail.com>
2024-10-03 00:09:34 +0530

    authentication setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
 require('dotenv').config();
 
 mongoose
@@ -29,5 +30,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
