f1d8a99de073dda6281487d9848112a05c6c369c
ankitraj-1602 <nycankit@gmail.com>
2024-10-03 17:10:40 +0530

    shopping product listing, filter and sort

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
+
+const shopProductsRouter = require("./routes/shop/products-routes");
 require('dotenv').config();
 
 mongoose
@@ -33,5 +35,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+
+app.use("/api/shop/products", shopProductsRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
