939bc1b3f56cb9d3d2b9ba1128a51a5d564564f1
ankitraj-1602 <nycankit@gmail.com>
2024-10-04 21:42:59 +0530

    working on home page for shopping view

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
-})
+    },
+  },
+  // Include .webp in assets handling
+  assetsInclude: ['**/*.webp'],
+});
